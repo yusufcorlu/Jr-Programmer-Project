c9b82dc8a19b6a506a4bf0b0b44f0a5e07ddacfc
yusufcorlu <yusufcorlu@hotmail.com>
2023-05-12 18:42:33 +0300

    initial

--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,9 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
